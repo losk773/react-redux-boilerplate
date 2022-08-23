1a3ae85d447c2dd3e627ab8ea578336c928f81e0
losk773 <losk773@gmail.com>
2022-08-23 17:01:04 +0300

    add webpack config and setup react

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Button } from "@/components/Button/Button";
+
+export const App = () => {
+  return (
+    <div>
+      React is setup <Button />
+    </div>
+  );
+};
