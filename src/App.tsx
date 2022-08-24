f610be1e006baac8a6310f381223ffa288776dc6
losk773 <losk773@gmail.com>
2022-08-24 08:48:53 +0300

    add eslint and prettier configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React from 'react';
 
-import { Button } from "@/components/Button/Button";
+import { Button } from '@/components/Button/Button';
 
 export const App = () => {
   return (
