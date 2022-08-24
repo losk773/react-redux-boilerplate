d769478384bfb7ab8826d4536a4bbcc433192ec9
losk773 <losk773@gmail.com>
2022-08-24 12:04:50 +0300

    update webpack config, eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
 import React from 'react';
 
-import { Button } from '@/components/Button/Button';
-
 export const App = () => {
-  return (
-    <div>
-      React is setup <Button />
-    </div>
-  );
+  return <div>React</div>;
 };
