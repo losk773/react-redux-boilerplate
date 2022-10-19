e3f7e4bda9e9860893d60995d6e7485857049c2e
losk773 <losk773@gmail.com>
2022-10-19 12:34:05 +0300

    removed extra selectors

--- src/features/Home/redux/home.selectors.ts
+++ src/features/Home/redux/home.selectors.ts
@@ -1,5 +1,3 @@
 import { State } from '@/common/redux/types';
 
 export const selectHome = ({ home }: State) => home;
-export const selectCounter = ({ home }: State) => home.counter;
-export const selectUsers = ({ home }: State) => home.users;
